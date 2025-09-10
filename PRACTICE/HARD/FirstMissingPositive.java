11aa7c832db805694f7d7600379c7a14c7305d28
83kRupali <rupaliramteke495@gmail.com>
2025-09-10 13:39:38 +0530

    Update FirstMissingPositive.java

--- PRACTICE/HARD/FirstMissingPositive.java
+++ PRACTICE/HARD/FirstMissingPositive.java
@@ -1,1 +1,21 @@
+class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        HashSet<Integer>set = new HashSet<>();
 
+        int large = Integer.MIN_VALUE;
+        for(int i:nums){
+            set.add(i);
+            large = Math.max(large, i);
+        }
+
+        if(large<0) return 1;
+        
+        for(int i=1; i<large; i++){
+            if(!set.contains(i)){
+                return i;
+            }
+        }
+        return large+1;
+
+    }
+}
