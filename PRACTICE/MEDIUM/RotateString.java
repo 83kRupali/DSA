c3bac59934c3c37b3ae4f9576cba89bf7017ee79
83kRupali <rupaliramteke495@gmail.com>
2025-10-02 14:11:11 +0530

    Create RotateString.java

---
+++ PRACTICE/MEDIUM/RotateString.java
@@ -0,0 +1,17 @@
+public boolean rotateString(String s, String goal) {
+        int n1 = s.length();
+        int n2 = goal.length();
+
+        if(n1 != n2) return false;
+        
+        boolean ans = false;
+        for(int j=0; j<n2; j++){
+            String str1 = goal.substring(0, j+1);
+            String str2 = goal.substring(j+1, n2);
+            String str = str2+str1;
+            if(s.equals(str)){
+                ans = true;
+            }
+        }
+        return ans;
+    }
