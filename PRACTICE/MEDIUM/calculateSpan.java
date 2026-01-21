fa549b70b978bb2fe2b01ce70c195370268f6f92
83kRupali <rupaliramteke495@gmail.com>
2026-01-21 12:34:38 +0530

    Update calculateSpan.java

--- PRACTICE/MEDIUM/calculateSpan.java
+++ PRACTICE/MEDIUM/calculateSpan.java
@@ -1,4 +1,11 @@
- public ArrayList<Integer> calculateSpan(int[] arr) {
+class Solution {
+    
+    /*
+        TC: O(n)
+        SC: O(n)
+    */
+    
+    public ArrayList<Integer> calculateSpan(int[] arr) {
         // code here
         Stack<Integer>stack = new Stack<>();
         ArrayList<Integer>res = new ArrayList<>();
@@ -17,6 +24,23 @@
             
             stack.push(i);
         }
-        
         return res;
     }
+ class StockSpanner {
+    /*
+    TC: O(n)
+    SC: O(n)
+     */
+    Stack<int[]>stack = new Stack<>();
+    public StockSpanner() {    
+    }
+    public int next(int price) {
+        int res = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price){
+            res+= stack.pop()[1];
+        }
+        stack.push(new int[]{price, res});
+        return res;
+    }   
+}
+}
